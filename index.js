3381945cf75a1a59d276b96dea98cfec27a4e052
Nurullo <thenurullo@gmail.com>
2021-05-11 01:34:31 -0600

    ADDED PDF MAKER FUNCTIONALITY

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-
 // The following will add the "add and remove" functionality of the webform
+
+
 
 $(document) .ready(function(){
     var NewInputField=`
     <div class="new-input">
+
+    <select name="room">
+    <option value="living"> Living Room </option>
+    <option value="dining"> Dining Room </option>
+    <option value="master"> Master Bedroom </option>
+    <option value="bed1"> Bedroom </option>
+    <option value="bonus"> Bonus Room </option>
+    
+  </select>
         Size
         <input type="number" name="num1" value="">
         x
@@ -25,3 +35,25 @@ $(document) .ready(function(){
     });
 
 });
+
+
+const generate = document.querySelector('.generate');
+
+generate.addEventListener('click', function(){
+    console.log('poggers');
+    createDoc();
+
+
+});
+
+function createDoc(){
+    var doc = {
+        content:[
+            "sup guys its Thenuruljlo here"
+        ]
+    }
+
+    createPdf(doc).download("quote");
+
+}
+
