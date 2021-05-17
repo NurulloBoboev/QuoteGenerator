674204ee0c172199cb4d39a53533cb45ba732439
Nurullo <thenurullo@gmail.com>
2021-05-17 15:26:55 -0600

    Working prototype of the PDF generator
    only works for casement at the moment

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ $(document) .ready(function(){
     <div class="new-input">
 
     <select name="room">
-    <option value="living"> Living Room </option>
-    <option value="dining"> Dining Room </option>
-    <option value="master"> Master Bedroom </option>
-    <option value="bed1"> Bedroom </option>
-    <option value="bonus"> Bonus Room </option>
+        <option value="Living Room"> Living Room </option>
+        <option value="Dining Room"> Dining Room </option>
+        <option value="Master Bedroom"> Master Bedroom </option>
+        <option value="Bedroom"> Bedroom </option>
+        <option value="Bonus Room"> Bonus Room </option>
     
-  </select>
+    </select>
         Size
         <input type="number" name="num1" value="" required>
         x
@@ -55,17 +55,34 @@ function createDoc(){
     var case_query = document.querySelectorAll('.casement_container .new-input');
     var case_length = case_query.length;
 
-    console.log(case_length);
+    var Awning_txt = "";
 
+    for( i = 0; i < case_length; i++){
+        var childs = case_query[i].childNodes;
+        console.log("This is the text in child[1]:" + childs[1].value);
+        console.log("This is the text in child[3]:" + childs[3].value);
+        console.log("This is the text in child[5]:" + childs[5].value);
+        console.log("This is the text in child[7]:" + childs[7].value);
+
+        var child_txt = childs[1].value + "\t\t\t" + childs[3].value+"\" x " + childs[5].value+"\""
+                        + "\t$" + childs[7].value + '\n';
+        Awning_txt += child_txt;
+
+    }
+
+    console.log(case_length);
+    console.log(Awning_txt);
 
 
     var doc = {
+        info: {
+            title: cus_name+ ' Window Quote'
+        },
         content:[
-
             {text: "WINDOW QUOTE", style: "header"},
             {text: "customer: " + cus_name, fontSize: 16},
-            {text: "email: " + cus_email, fontSize: 16}
-
+            {text: "email: " + cus_email, fontSize: 16},
+            {text: "\n\n\n"+Awning_txt, fontSize: 12}
 
 
         ],
@@ -77,7 +94,9 @@ function createDoc(){
         }
     }
 
-    createPdf(doc).download("quote");
+    var win = window.open('hello', '_blank');
+
+    createPdf(doc).open({}, win);
 
 }
 
